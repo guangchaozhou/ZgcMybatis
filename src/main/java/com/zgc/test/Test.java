1a1c232559373b4d2a17845f240ae5d7dc0a4849
周广超 <10761623+zhou-guangchao@user.noreply.gitee.com>
2022-08-01 08:37:05 +0800

    init

--- src/main/java/com/zgc/test/Test.java
+++ src/main/java/com/zgc/test/Test.java
@@ -10,8 +10,8 @@ public class Test {
     public static void main(String[] args) {
 
         UserMapper mapper = MapperProxyFactory.getMapper(UserMapper.class);
-        List<User> zhouyu = mapper.getUser("zhouyu");
-        System.out.println(zhouyu);
+        List<User> zgc = mapper.getUser("zgc");
+        System.out.println(zgc);
 
         User user = mapper.getUserById(1);
         System.out.println(user);
